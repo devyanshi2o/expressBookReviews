9f83d0cee3918209a2086b31ef28c2bed903c7f8
Devyanshi vyas <devyanshivyas02@gmail.com>
2026-05-07 01:06:07 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,119 +2,125 @@ const express = require('express');
 const public_users = express.Router();
 
 const axios = require('axios');
-
-let books = {
-    "1": {
-        "author": "Chinua Achebe",
-        "title": "Things Fall Apart",
-        "reviews": {}
-    },
-    "2": {
-        "author": "Hans Christian Andersen",
-        "title": "Fairy tales",
-        "reviews": {}
-    },
-    "3": {
-        "author": "Dante Alighieri",
-        "title": "The Divine Comedy",
-        "reviews": {}
-    }
-};
+let books = require("./booksdb.js");
 
 // Get all books
 public_users.get('/', function (req, res) {
     return res.status(200).json(books);
 });
 
-// Get book details based on ISBN
+// Get by ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if (book) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
+    return res.status(200).json(books[isbn]);
 });
 
-// Get book details based on author
+// Get by author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
 
-    const filteredBooks = Object.values(books).filter(
-        book => book.author.toLowerCase() === author.toLowerCase()
-    );
+    const filteredBooks = Object.keys(books)
+        .filter(key => books[key].author === author)
+        .reduce((result, key) => {
+            result[key] = books[key];
+            return result;
+        }, {});
 
     return res.status(200).json(filteredBooks);
 });
 
-// Get all books based on title
+// Get by title
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
 
-    const filteredBooks = Object.values(books).filter(
-        book => book.title.toLowerCase() === title.toLowerCase()
-    );
+    const filteredBooks = Object.keys(books)
+        .filter(key => books[key].title === title)
+        .reduce((result, key) => {
+            result[key] = books[key];
+            return result;
+        }, {});
 
     return res.status(200).json(filteredBooks);
 });
 
-// Get book review
+// Get reviews
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-
-    if (books[isbn]) {
-        return res.status(200).json(books[isbn].reviews);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
+    return res.status(200).json(books[isbn].reviews);
 });
 
-// Task 10: Get all books using async callback function
+// Task 10
 public_users.get('/async/books', async function (req, res) {
+
     try {
+
         const response = await axios.get('http://localhost:5000/');
+
         return res.status(200).json(response.data);
+
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
-// Task 11: Search by ISBN using Promises
-public_users.get('/async/isbn/:isbn', function (req, res) {
+// Task 11
+public_users.get('/promise/isbn/:isbn', function (req, res) {
+
     const isbn = req.params.isbn;
 
     axios.get(`http://localhost:5000/isbn/${isbn}`)
         .then(response => {
+
             return res.status(200).json(response.data);
+
         })
         .catch(error => {
-            return res.status(500).json({ message: error.message });
+
+            return res.status(500).json({
+                message: error.message
+            });
+
         });
 });
 
-// Task 12: Search by Author
+// Task 12
 public_users.get('/async/author/:author', async function (req, res) {
+
     const author = req.params.author;
 
     try {
+
         const response = await axios.get(`http://localhost:5000/author/${author}`);
+
         return res.status(200).json(response.data);
+
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
-// Task 13: Search by Title
+// Task 13
 public_users.get('/async/title/:title', async function (req, res) {
+
     const title = req.params.title;
 
     try {
+
         const response = await axios.get(`http://localhost:5000/title/${title}`);
+
         return res.status(200).json(response.data);
+
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
